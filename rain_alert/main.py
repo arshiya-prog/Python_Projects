51825351913bc12138a1a4c592216ea0129b4ff2
Arshiya Uppal <arshiya.uppal@aeronsystems.com>
2026-09-04 12:12:21 +0530

    Modified the main.py file to integrate Twilio

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,14 +1,31 @@
 import requests
+from twilio.rest import Client
+import os
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 parameters = {
     "lat" : 18.520430,
     "lon" : 73.856743,
-    "appid" : "4f6a41c787221da11589d8b2ac8d5cf2",
+    "appid" : os.environ["OWM_API_KEY"],
     "cnt" : 4,
 }
 
-data = requests.get(url=OWM_endpoint, params=parameters)
-# print(data.status_code)
-response = data.json()
-print(response)
+response = requests.get(url=OWM_endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an umbrella ☔️.",
+        from_=os.environ["TWILIO_FROM_NUMBER"],
+        to=os.environ["MY_PHONE_NUMBER"],
+    )
+    print(message.status)
